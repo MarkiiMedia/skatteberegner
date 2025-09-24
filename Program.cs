e0bdbac80097cddd0ca2d31490f81db0c4252e64
Mark Mundus Andersen <31654229+MarkiiMedia@users.noreply.github.com>
2025-09-24 11:12:20 +0200

    initial commit
    ini comm med random test projekt bare fordi.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using Transport;
+
+Console.WriteLine("Mundus første");
+
+//Prøvet leg med namespace, kender ikke C# nok, men det bruges også i min nye Drupal version så den går jeg med.
+namespace Transport
+{
+
+    public class TransportBeregner
+    {
+        public int BeregnPris(double afstandKm, double vaegtKg)
+        {
+            Console.WriteLine($"Beregner pris for afstand: {afstandKm}km og vægt: {vaegtKg}kg");
+            if (afstandKm < 5)
+            {
+                if (vaegtKg < 10)
+                {
+                    Console.WriteLine("Transporten er gratis.");
+                    return 0;
+                }
+                else
+                    Console.WriteLine("Transport pris: 50kr");
+                return 50;
+            }
+            else
+            {
+                if (vaegtKg < 10)
+                {
+                    Console.WriteLine("Transport pris: 75kr");
+                    return 75;
+                }
+                else
+                    Console.WriteLine("Transport pris: 100kr");
+                return 100;
+            }
+        }
+    }
+}
